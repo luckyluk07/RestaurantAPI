35372e7194427752b458a78ac19c9d4dbaa8a017
luckyluk07 <L.Nojman11@wp.pl>
2022-02-23 23:40:01 +0100

    Create address model class

---
+++ src/main/java/pl/nojman/restaurant_api/Models/Address.java
@@ -0,0 +1,81 @@
+package pl.nojman.restaurant_api.Models;
+
+import java.util.Objects;
+
+public class Address {
+    private Long id;
+    private String city;
+    private String street;
+    private String number;
+
+    public Address() {
+    }
+
+    public Address(Long id, String city, String street, String number) {
+        this.id = id;
+        this.city = city;
+        this.street = street;
+        this.number = number;
+    }
+
+    public Address(String city, String street, String number) {
+        this.city = city;
+        this.street = street;
+        this.number = number;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return id.equals(address.id) && city.equals(address.city) && street.equals(address.street) && number.equals(address.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, city, street, number);
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", number='" + number + '\'' +
+                '}';
+    }
+}
