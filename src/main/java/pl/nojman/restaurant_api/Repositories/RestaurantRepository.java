e8e37634548d70158758b64cde288ee0bda27938
luckyluk07 <L.Nojman11@wp.pl>
2022-02-24 19:29:11 +0100

    Add basic restaurant repository

--- src/main/java/pl/nojman/restaurant_api/Repositories/RestaurantRepository.java
+++ src/main/java/pl/nojman/restaurant_api/Repositories/RestaurantRepository.java
@@ -1,4 +1,51 @@
 package pl.nojman.restaurant_api.Repositories;
 
+import pl.nojman.restaurant_api.Models.Restaurant;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class RestaurantRepository {
+    private Set<Restaurant> restaurants = new HashSet<>();
+
+    public List<Restaurant> getAll() {
+        return new ArrayList<>(restaurants);
+    }
+
+    public Restaurant get(Long id) {
+        return restaurants.stream()
+                .filter(restaurant -> restaurant.getId() == id)
+                .findFirst()
+                .get();
+    }
+
+    public boolean delete(Long id) {
+        Restaurant restaurant = restaurants.stream()
+                .filter(r -> r.getId() == id)
+                .findFirst()
+                .get();
+        if (restaurants.contains(restaurant)) {
+            restaurants.remove(restaurant);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean update(Restaurant restaurant) {
+        Restaurant existingRestaurant = restaurants.stream()
+                .filter(r -> r.getId() == restaurant.getId())
+                .findFirst()
+                .get();
+        if (restaurants.contains(existingRestaurant)) {
+            existingRestaurant = restaurant;
+            return true;
+        }
+        return false;
+    }
+
+    public void create(Restaurant restaurant) {
+        restaurants.add(restaurant);
+    }
 }
