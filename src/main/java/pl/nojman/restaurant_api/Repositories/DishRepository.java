72cd33f25c46712189350876724b91b512810969
luckyluk07 <L.Nojman11@wp.pl>
2022-02-24 19:19:34 +0100

    Init basic dish repository

---
+++ src/main/java/pl/nojman/restaurant_api/Repositories/DishRepository.java
@@ -0,0 +1,49 @@
+package pl.nojman.restaurant_api.Repositories;
+
+import pl.nojman.restaurant_api.Models.Dish;
+
+import java.util.*;
+
+public class DishRepository {
+
+    private Set<Dish> dishes = new HashSet<>();
+
+    public List<Dish> getAll() {
+        return new ArrayList<>(dishes);
+    }
+
+    public Dish get(Long id) {
+        return dishes.stream()
+                .filter(dish -> dish.getId() == id)
+                .findFirst()
+                .get();
+    }
+
+    public boolean delete(Long id) {
+        Dish dish = dishes.stream()
+                .filter(d -> d.getId() == id)
+                .findFirst()
+                .get();
+        if (dishes.contains(dish)) {
+            dishes.remove(dish);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean update(Dish dish) {
+        Dish existingDish = dishes.stream()
+                .filter(d -> d.getId() == dish.getId())
+                .findFirst()
+                .get();
+        if (dishes.contains(existingDish)) {
+            existingDish = dish;
+            return true;
+        }
+        return false;
+    }
+
+    public void create(Dish dish) {
+        dishes.add(dish);
+    }
+}
---
+++ src/main/java/pl/nojman/restaurant_api/Repositories/RestaurantRepository.java
@@ -0,0 +1,4 @@
+package pl.nojman.restaurant_api.Repositories;
+
+public class RestaurantRepository {
+}
