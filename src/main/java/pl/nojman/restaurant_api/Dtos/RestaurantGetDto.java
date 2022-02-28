aabb120bdd7a4e27525e33326da0095584766ce0
luckyluk07 <L.Nojman11@wp.pl>
2022-02-28 20:43:52 +0100

    Create get restaurant dto

---
+++ src/main/java/pl/nojman/restaurant_api/Dtos/RestaurantGetDto.java
@@ -0,0 +1,10 @@
+package pl.nojman.restaurant_api.Dtos;
+
+public class RestaurantGetDto {
+    private String name;
+    private String description;
+
+    private String city;
+    private String street;
+    private String number;
+}
