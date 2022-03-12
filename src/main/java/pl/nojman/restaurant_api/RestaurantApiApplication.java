9cbdf2bacf91f0121c384f2c5fcfbf36fb90551e
luckyluk07 <L.Nojman11@wp.pl>
2022-03-12 22:20:07 +0100

    Add basic swagger2 documentation

--- src/main/java/pl/nojman/restaurant_api/RestaurantApiApplication.java
+++ src/main/java/pl/nojman/restaurant_api/RestaurantApiApplication.java
@@ -2,8 +2,10 @@ package pl.nojman.restaurant_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RestaurantApiApplication {
 
 	public static void main(String[] args) {
